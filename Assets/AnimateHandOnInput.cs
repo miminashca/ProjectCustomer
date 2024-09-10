e7cf957c84489992f8d619cbadf8407861f03a34
Thijmnn <151636889+Thijmnn@users.noreply.github.com>
2024-09-10 12:50:16 +0200

    Hands in. (animations working on)

---
+++ Assets/AnimateHandOnInput.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AnimateHandOnInput : MonoBehaviour
+{
+    [SerializeField] private InputActionProperty pinchAnim;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
