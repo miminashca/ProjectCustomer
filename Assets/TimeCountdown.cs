a5bf2746d537095896423d55a2cb25a52028c47a
Thijmnn <151636889+Thijmnn@users.noreply.github.com>
2024-09-19 17:21:30 +0200

    changes to build and run

--- Assets/TimeCountdown.cs
+++ Assets/TimeCountdown.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
-using UnityEditor.ShaderKeywordFilter;
+
 public class TimeCountdown : MonoBehaviour
 {
     [SerializeField] Image black;
