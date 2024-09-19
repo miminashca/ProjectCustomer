627b549497fe615d2329e8ec50ef385cfd09434b
Connor Smith <545278@student.saxion.nl>
2024-09-19 14:48:17 +0200

    PhoneImplemented

---
+++ Assets/Important/Scripts/Temporary/PhoneCheckmarkScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhoneCheckmarkScript : MonoBehaviour
+{
+    private Image checkMark;
+    [SerializeField] private int ThisID;
+
+    private void Awake()
+    {
+        checkMark = GetComponent<Image>();
+        checkMark.enabled = false; 
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        QuestManager.questUpdated += checkOff;
+    }
+
+    private void checkOff(bool done, int ID)
+    {
+        if (done)
+        {
+            if(ID == ThisID)
+            {
+                checkMark.enabled = true;
+
+            }
+        }
+        else
+        {
+            checkMark.enabled=false;
+        }
+    }
+}
